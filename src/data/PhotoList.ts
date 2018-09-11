b1017544a96df4656228faa1304e379c01212d3a
Bai Yunfei <baiyunfeiii@foxmali.com>
2018-09-11 23:17:46 +0800

    Bind comment data to view

--- src/data/PhotoList.ts
+++ src/data/PhotoList.ts
@@ -1,24 +1,42 @@
 
 const photoList: any = [{
-    nickName: 'Yunfei Bai',
+    nickName: 'Yunfei',
     avatar: '/assets/avatar2.jpg',
-    image: '/assets/image3.jpg',
+    image: '/assets/image1.png',
     upQuantity: 9283,
-    marked: false
+    marked: false,
+    comments: [
+      {
+        nickName: 'worldstar',
+        content: 'Y’all really doing this?! 😂💀 #WSHH'
+      }
+    ]
   },
   {
-    nickName: 'Ming Liu',
+    nickName: 'Ming',
     avatar: '/assets/avatar3.jpg',
     image: '/assets/image2.jpg',
     upQuantity: 90,
-    marked: false
+    marked: false,
+    comments: [
+      {
+        nickName: 'worldstar',
+        content: 'Y’all really doing this?! 😂💀 #WSHH'
+      }
+    ]
   },
   {
     nickName: 'Xiang',
     avatar: '/assets/avatar4.jpg',
-    image: '/assets/image1.png',
+    image: '/assets/image3.jpg',
     upQuantity: 23578,
-    marked: false
+    marked: false,
+    comments: [
+      {
+        nickName: 'worldstar',
+        content: 'Y’all really doing this?! 😂💀 #WSHH'
+      }
+    ]
   }];
 
 export default photoList;
